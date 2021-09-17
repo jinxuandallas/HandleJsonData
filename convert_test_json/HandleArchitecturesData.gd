85a825eecbf96c8bf05679d05e27fba285060818
Dallas <dallas2k@163.com>
2021-09-17 15:11:49 +0800

    增加了处理郡的逻辑

--- convert_test_json/HandleArchitecturesData.gd
+++ convert_test_json/HandleArchitecturesData.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var arc_arr:Array
+var jun_arr:Array
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,8 +15,53 @@ func _ready():
 	else:
 		_merge_architectures_data()
 		
-	_handle_architectures_data()
+	_handle_jun()
+#	_handle_architectures_data()
 #	print(arc_arr)
+
+# 处理生成郡的数据
+func _handle_jun():
+	var id=0
+	for arc in arc_arr:
+		if _is_new_jun(arc["Jun"]):
+			var junzhi_id=-1
+			if bool(arc["JunZhi"])==true:
+				junzhi_id=int(arc["Id"])
+			jun_arr.append({"Id":id,"Name":arc["Jun"],"JunZhi":junzhi_id})
+			id+=1
+			continue
+		if bool(arc["JunZhi"])==true:
+			_find_jun(arc)
+			
+	var file=File.new()
+	file.open("res://convert_test_json/new_data/Jun.json",File.WRITE)
+	file.store_string(JSON.print(jun_arr,"\t"))
+	file.close()
+#	print(jun_arr)
+
+
+# 设置郡治	
+func _find_jun(arc):
+	for jun in jun_arr:
+		if jun["Name"]==arc["Jun"]:
+			jun["JunZhi"]=int(arc["Id"])
+			break
+
+# 判断郡是否是新郡	
+func _is_new_jun(jun_name):
+	if jun_name=="":
+		return false
+	
+	if jun_arr.size()==0:
+		return true
+		
+	for jun in jun_arr:
+		if jun["Name"]==jun_name:
+			return false
+	
+	return true
+	
+			
 func _handle_architectures_data():
 	var add_dataname=["Agriculture","Commerce","Endurance","Food","Fund","Population","PersonsString"]
 	var file=File.new()
